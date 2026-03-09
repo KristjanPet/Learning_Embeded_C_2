c089e304518bf5fe5a15b00b13be631ad6ee13fd
Kristjan <kristjan.pet@gmail.com>
2026-03-09 11:08:39 +0100

    Refactor CMakeLists and main.cpp for including blink library

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,13 @@
-#include <stdio.h>
-#include <vector>
-#include <string>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
-#include "esp_heap_caps.h"
-#include <cstring>
-
-static const char* TAG = "mem_demo";
-
-static int bad_return(int x) {
-    if (x > 0) return 1;
-    // missing return on purpose
-}
+#include "blink_lib.hpp"
 
 extern "C" void app_main(void)
 {
+    blink_lib::init(GPIO_NUM_2);
 
     while (true) {
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        blink_lib::toggle();
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
